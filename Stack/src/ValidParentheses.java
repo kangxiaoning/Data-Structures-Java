59c39693f16163520b1e24bad0e19bc264facd18
dbo <kxnmei@163.com>
2018-04-29 11:53:33 +0800

    使用自定义Stack实现有效的括号题目解答，通过leetcode验证数据结构

---
+++ Stack/src/ValidParentheses.java
@@ -0,0 +1,45 @@
+/**
+ * leetcode题目：https://leetcode-cn.com/problems/valid-parentheses/description/
+ * 方法一：使用java自带的Stack实现
+ */
+
+class ValidParentheses {
+    public boolean isValid(String s) {
+
+        ArrayStack<Character> stack = new ArrayStack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char topChar = stack.pop();
+                if (c == ')' && topChar != '(') {
+                    return false;
+                }
+                if (c == ']' && topChar != '[') {
+                    return false;
+                }
+                if (c == '}' && topChar != '{') {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+
+        // https://leetcode-cn.com/problems/valid-parentheses/description/
+        // 示例
+
+        System.out.println((new ValidParentheses()).isValid("()"));
+        System.out.println((new ValidParentheses()).isValid("()[]{}"));
+        System.out.println((new ValidParentheses()).isValid("(]"));
+        System.out.println((new ValidParentheses()).isValid("([)]"));
+        System.out.println((new ValidParentheses()).isValid("{[]}"));
+    }
+}
