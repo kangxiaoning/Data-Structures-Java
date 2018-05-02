63bbfc636decf747f045abe6e359f15063a986b5
kangxiaoning <kxnmei@163.com>
2018-05-02 20:01:09 +0800

    验证链表增删改查

--- LinkedList/src/Main.java
+++ LinkedList/src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for (int i = 0; i < 5; i++) {
+            // 对链表来说addFirst是O(1)级别，对数组来书addLast是O(1)级别的
+            linkedList.addFirst(i);
+            System.out.println(linkedList);
+        }
+
+        linkedList.add(2, 666);
+        System.out.println(linkedList);
+
+        linkedList.remove(2);
+        System.out.println(linkedList);
+
+        linkedList.removeFirst();
+        System.out.println(linkedList);
+
+        linkedList.removeLast();
+        System.out.println(linkedList);
     }
 }
