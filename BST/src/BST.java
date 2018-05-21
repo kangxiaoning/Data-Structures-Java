89941a8decca5dc6561982e500a726634261c6cb
kangxiaoning <kxnmei@163.com>
2018-05-21 19:29:43 +0800

    二分搜索树添加元素实现

--- BST/src/BST.java
+++ BST/src/BST.java
@@ -25,4 +25,27 @@ public class BST<E extends Comparable<E>> {
     public boolean isEmpty() {
         return size == 0;
     }
+
+    // 向二分搜索树添加新的元素e
+    public void add(E e) {
+        root = add(root, e);
+
+    }
+
+    // 向以node为根的二分搜索树中插入元素e，递归算法
+    // 返回插入新节点后二分搜索树的根
+    private Node add(Node node, E e) {
+
+        if (node == null) {
+            size++;
+            return new Node(e);
+        }
+
+        if (e.compareTo(node.e) < 0) {
+            node.left = add(node.left, e);
+        } else if (e.compareTo(node.e) > 0) {   // 这里不能用else，这是因为之前没有判断e和node.e相等的情况
+            node.right = add(node.right, e);    // 这里对相等的情况其实等于什么都不做
+        }
+        return node;
+    }
 }
