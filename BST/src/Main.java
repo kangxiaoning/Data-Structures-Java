0cc170332ce789e2917531dac05127962c0c7a6f
kangxiaoning <kxnmei@163.com>
2018-06-06 14:21:15 +0800

    二分搜索树删除最大值和最小值

--- BST/src/Main.java
+++ BST/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -31,5 +34,40 @@ public class Main {
         System.out.println();
 
 //        System.out.println(bst);
+
+        // test removeMin
+        BST<Integer> bst1 = new BST<>();
+        Random random = new Random();
+        int n = 1000;
+        for (int i = 0; i < n; i++) {
+            bst1.add(random.nextInt(10000));
+        }
+        ArrayList<Integer> nums1 = new ArrayList<>();
+        while (!bst1.isEmpty()) {
+            nums1.add(bst1.removeMin());
+        }
+        System.out.println(nums1);
+        for (int i = 1; i < nums1.size(); i++) {
+            if (nums1.get(i - 1) > nums1.get(i)) {
+                throw new IllegalArgumentException("Error");
+            }
+        }
+        System.out.println("removeMin test completed.");
+
+        // test removeMax
+        for (int i = 0; i < n; i++) {
+            bst1.add(random.nextInt(10000));
+        }
+        nums1 = new ArrayList<>();
+        while (!bst1.isEmpty()) {
+            nums1.add(bst1.removeMax());
+        }
+        System.out.println(nums1);
+        for (int i = 1; i < nums1.size(); i++) {
+            if (nums1.get(i - 1) < nums1.get(i)) {
+                throw new IllegalArgumentException("Error");
+            }
+        }
+        System.out.println("removeMax test completed.");
     }
 }
